e23085f5f8fd67996beb2077c05155a9f6b8e5d0
Sabisa Siganga <lumisabisa@gmail.com>
2024-02-07 13:58:24 +0200

    Created post details page

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,51 +1,103 @@
-"use client";
+'use client';
 
-import Favourites from "@/app/components/Favourites/Favourites";
-import Link from "next/link";
-import React from "react";
-import { Col, FloatingLabel, Form, Row } from "react-bootstrap";
+import Favorites from '@/app/components/Favorites/Favorites';
+import Link from 'next/link';
+import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import styles from './styles.module.scss';
+import CommentBox from '@/app/components/CommentBox/CommentBox';
 
-const fetchPostDetails = () => {
-  return (
-    <div className="detailsContainer">
-      <Row>
-        <Col sm={8}>
-          <div>
-            <div className="homeBtn">
-              <Link href="/">Back home</Link>
-            </div>
-            <div className="detailsContainer">
-              <p>Author: Emily Jones</p>
-              <p>
-                Brook was found killed and her killers have left the country,
-                Police are still in search of the killers
-              </p>
-              <div>
-                <p>Comments</p>
-                <p>post comments</p>
+const PostDetails = () => {
+	const [post, setPost] = React.useState({
+		id: 1,
+		title: 'this is my title',
+		author: 'Emily Jones',
+		body: 'Brook was found killed and her killers have left the country, Police are still in search of the killers',
+	});
 
-                <div>
-                  <FloatingLabel controlId="floatingTextarea2" label="Comments">
-                    <Form.Control
-                      as="textarea"
-                      placeholder="Leave a comment here"
-                      style={{ height: "100px" }}
-                    />
-                  </FloatingLabel>
-                </div>
-              </div>
-            </div>
-          </div>
-        </Col>
+	const [comments, setComments] = React.useState([
+		// {
+		// 	comment:
+		// 		'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime rem repellendus temporibus ut eaque perspiciatis saepe quis similique consectetur nesciunt, incidunt distinctio qui harum iusto quo sed, magni totam accusantium.',
+		// 	author: 'Gary Mariner',
+		// },
+		// {
+		// 	comment:
+		// 		'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime rem repellendus temporibus ut eaque perspiciatis saepe quis similique consectetur nesciunt, incidunt distinctio qui harum iusto quo sed, magni totam accusantium.',
+		// 	author: 'Gary Mariner',
+		// },
+		// {
+		// 	comment:
+		// 		'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime rem repellendus temporibus ut eaque perspiciatis saepe quis similique consectetur nesciunt, incidunt distinctio qui harum iusto quo sed, magni totam accusantium.',
+		// 	author: 'Gary Mariner',
+		// },
+	]);
 
-        <Col sm={4}>
-          <div className="favourites">
-            <Favourites />
-          </div>
-        </Col>
-      </Row>
-    </div>
-  );
+	return (
+		<div className='px-5'>
+			<Row>
+				<Col sm={8}>
+					<div className='px-5'>
+						<h1 className='pb-4 pt-5 mb-0'>Post: {post.title}</h1>
+
+						<div>
+							<Link href='/' className='btn btn-primary'>
+								Back: Home
+							</Link>
+						</div>
+						<div className='py-5'>
+							<div className={styles.author}>
+								<span>Author:</span> {post.author}
+							</div>
+							<div className={styles.textBody}>{post.body}</div>
+							<div>
+								<div className={styles.commentTitle}>
+									<span>Comments</span>
+								</div>
+								<div className={styles.commentsContainer}>
+									{comments.map((comment, index) => {
+										return (
+											<div
+												key={index}
+												className={styles.comment}
+											>
+												<div
+													className={
+														styles.commentAuthor
+													}
+												>
+													{comment.author}
+												</div>
+												<div
+													className={
+														styles.commentText
+													}
+												>
+													{comment.comment}
+												</div>
+											</div>
+										);
+									})}
+
+									{comments.length === 0 && (
+										<div className={styles.noComments}>
+											No comments yet
+										</div>
+									)}
+								</div>
+							</div>
+
+							<CommentBox />
+						</div>
+					</div>
+				</Col>
+
+				<Col sm={4}>
+					<Favorites />
+				</Col>
+			</Row>
+		</div>
+	);
 };
 
-export default fetchPostDetails;
+export default PostDetails;
